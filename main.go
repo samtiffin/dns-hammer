9876274480dfe4a2f529b0a3ef24ad9c1047f719
samtiffin <sam@samtiffin.com>
2024-03-29 13:55:09 +0000

    Make things a bit nicer...

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -41,37 +42,63 @@ type Stats struct {
 }
 
 func (s Stats) String() string {
-	return fmt.Sprintf("total: %d min: %s max: %s avg: %s errors: %d",
-		s.count,
+	return fmt.Sprintf("total: %s min: %s max: %s avg: %s requests: %d errors: %d",
+		s.total,
 		s.min,
 		s.max,
 		s.avg,
+		s.count,
 		s.errCount,
 	)
 }
 
 type Config struct {
-	domains       domains
-	concurrency   int
-	blows         int
-	statsInterval time.Duration
-	abortAfter    time.Duration
+	Domains       domains
+	Concurrency   int
+	Blows         int
+	StatsInterval time.Duration
+	// TODO: not implemented
+	// AbortAfter    time.Duration
 }
 
 func (c *Config) Validate() error {
-	return nil
+	errs := []error{}
+
+	if len(c.Domains) == 0 {
+		errs = append(errs, errors.New("must provide at least one domain"))
+	}
+
+	if c.Concurrency < 1 {
+		errs = append(errs, errors.New("concurrency must be >= 1"))
+	}
+
+	if c.Blows < 0 {
+		errs = append(errs, errors.New("blows must be a positive integer"))
+	}
+
+	if c.StatsInterval < 0 {
+		errs = append(errs, errors.New("stats interval must be a positive duration"))
+	}
+
+	// TODO: not implemented
+	//if c.AbortAfter < 0 {
+	//  err = append(errs, errors.New("abort after must be a positive duration"))
+	//}
+
+	return errors.Join(errs...)
 }
 
 func info(out io.Writer, cfg Config) {
 	fmt.Fprintln(out, "╔══════════════════════╗")
 	fmt.Fprintln(out, "║ 🔨 DNS HAMMER v0.0.1 ║")
 	fmt.Fprintln(out, "╚══════════════════════╝")
-	fmt.Fprintf(out, "> Domains: %s\n", strings.Join(cfg.domains, "\n          "))
-	fmt.Fprintf(out, "> Hammer Blows: %d\n", cfg.blows)
-	fmt.Fprintf(out, "> Concurrency: %d\n", cfg.concurrency)
-	fmt.Fprintf(out, "> Stats Interval: %s\n", cfg.statsInterval)
-	fmt.Fprintf(out, "> Abort After: %s\n", cfg.abortAfter)
-	fmt.Fprintln(out, "")
+	fmt.Fprintf(out, "> Domains: %s\n", strings.Join(cfg.Domains, "\n           "))
+	fmt.Fprintf(out, "> Hammer Blows: %d\n", cfg.Blows)
+	fmt.Fprintf(out, "> Concurrency: %d\n", cfg.Concurrency)
+	fmt.Fprintf(out, "> Stats Interval: %s\n", cfg.StatsInterval)
+	// TODO: not implemented
+	// fmt.Fprintf(out, "> Abort After: %s\n", cfg.AbortAfter)
+	fmt.Fprintln(out, "════╣ HAMMER TIME ╠════")
 }
 
 func resolver(ctx context.Context, r *net.Resolver, domains <-chan string, results chan<- Result) {
@@ -89,11 +116,12 @@ func run(ctx context.Context, out io.Writer, args []string) error {
 	cfg := Config{}
 
 	flags := flag.NewFlagSet(args[0], flag.ContinueOnError)
-	flags.Var(&cfg.domains, "domain", "TODO")
-	flags.IntVar(&cfg.blows, "blows", DefaultBlows, "TODO")
-	flags.IntVar(&cfg.concurrency, "concurrency", runtime.NumCPU(), "TODO")
-	flags.DurationVar(&cfg.statsInterval, "stats-interval", DefaultStatsInterval, "TODO")
-	flags.DurationVar(&cfg.abortAfter, "abort-after", DefaultAbortAfter, "TODO")
+	flags.Var(&cfg.Domains, "domain", "Domain(s) to use to hammer DNS server with. Pass -domain arg again for each domain.")
+	flags.IntVar(&cfg.Blows, "blows", DefaultBlows, "Number of times to hit the DNS server.")
+	flags.IntVar(&cfg.Concurrency, "concurrency", runtime.NumCPU(), "How many hammers to use at once, defaults to number of cpus.")
+	flags.DurationVar(&cfg.StatsInterval, "stats-interval", DefaultStatsInterval, "How often to report stats.")
+	// TODO: not implemented
+	// flags.DurationVar(&cfg.AbortAfter, "abort-after", DefaultAbortAfter, "Abort hammering if a request exceeds this time (NOT IMPLEMENTED")
 
 	if err := flags.Parse(args[1:]); err != nil {
 		return err
@@ -108,14 +136,14 @@ func run(ctx context.Context, out io.Writer, args []string) error {
 
 	info(out, cfg)
 
-	d := make(chan string, cfg.blows)
-	res := make(chan Result, cfg.blows)
+	d := make(chan string, cfg.Blows)
+	res := make(chan Result, cfg.Blows)
 
-	for worker := 0; worker < cfg.concurrency; worker++ {
+	for worker := 0; worker < cfg.Concurrency; worker++ {
 		go resolver(ctx, r, d, res)
 	}
 
-	ticker := time.NewTicker(cfg.statsInterval)
+	ticker := time.NewTicker(cfg.StatsInterval)
 	tickerDone := make(chan bool)
 
 	go func() {
@@ -129,13 +157,13 @@ func run(ctx context.Context, out io.Writer, args []string) error {
 		}
 	}()
 
-	for i := 0; i < cfg.blows; i++ {
+	for i := 0; i < cfg.Blows; i++ {
 		// cycle through domains in list...
-		d <- cfg.domains[i%len(cfg.domains)]
+		d <- cfg.Domains[i%len(cfg.Domains)]
 	}
 	close(d)
 
-	for a := 0; a < cfg.blows; a++ {
+	for a := 0; a < cfg.Blows; a++ {
 		result := <-res
 
 		stats.total += result.duration
@@ -156,6 +184,7 @@ func run(ctx context.Context, out io.Writer, args []string) error {
 	ticker.Stop()
 	tickerDone <- true
 	fmt.Fprintln(out, stats.String())
+	fmt.Fprintln(out, "════╣ STOP ╠════")
 	return nil
 }
 
